39326fa10c02e51e7b8326dd7b765eee79008b15
lengoc12 <52289366+lengoc12@users.noreply.github.com>
2023-04-28 15:00:38 +0700

    ,

--- Webapp/Users/serializers.py
+++ Webapp/Users/serializers.py
@@ -1,11 +1,18 @@
 from django.urls import path, include
 from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
-
+from rest_framework import routers, serializers
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'is_staff', 'first_name', 'last_name']
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
 
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(user.password)
+        user.save()
+
+        return user
